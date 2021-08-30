de9dbba38621a2a88211f393872813a03fdc2fa6
Burutsako24 <77276546+Burutsako24@users.noreply.github.com>
2021-08-30 22:01:17 +0700

    Create MainActivity.java

---
+++ Test5/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.test5;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+            }
+        });
+        Button btn1 = (Button)findViewById(R.id.button4);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MainActivity3.class);
+                startActivity(i);
+            }
+        });
+        Button btn3 = (Button)findViewById(R.id.button9);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MainActivity4.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+}
